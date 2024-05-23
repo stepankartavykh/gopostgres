bbfae8c396ab1a875f7c503934f420bbc0bb309a
sklion <sktvpx@gmail.com>
2024-05-23 20:02:28 +0300

    Adding file for handling queries from client.

--- main.go
+++ main.go
@@ -18,33 +18,40 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("listening on %s, prefix: %s\n", listener.Addr(), prefix)
-
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("failed to accept connection, err:", err)
 			continue
 		}
-
 		go handleConnection(conn, prefix)
 	}
 }
 
 func handleConnection(conn net.Conn, prefix string) {
-	defer conn.Close()
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println(conn.LocalAddr())
+			fmt.Println(conn.RemoteAddr())
+		}
+	}(conn)
 	reader := bufio.NewReader(conn)
 	for {
-		bytes, err := reader.ReadBytes(byte('\n'))
+		requestQuery, err := reader.ReadBytes(byte('\n'))
 		if err != nil {
 			if err != io.EOF {
 				fmt.Println("failed to read data, err:", err)
 			}
 			return
 		}
-		fmt.Printf("request: %s", bytes)
-
-		line := fmt.Sprintf("%s %s", prefix, bytes)
+		fmt.Printf("request: %s", requestQuery)
+		handleQuery(string(requestQuery))
+		line := fmt.Sprintf("%s %s", prefix, requestQuery)
 		fmt.Printf("response: %s", line)
-		conn.Write([]byte(line))
+		write, err := conn.Write([]byte(line))
+		if err != nil {
+			fmt.Println(write)
+		}
 	}
 }
---
+++ query.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func handleQuery(query string) {
+	fmt.Println("processing query ...", query)
+}
