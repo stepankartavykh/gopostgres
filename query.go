f7552e6516170b5743018f346734dcf56cf50426
sklion <sktvpx@gmail.com>
2024-05-24 20:28:45 +0300

    Adding basic logic for processing query from client.

--- query.go
+++ query.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
+type StructuredQuery struct {
+}
+
+type ExecutionResult struct {
+	status string
+}
+
+func (r ExecutionResult) getDebugMessage() string {
+	return r.status
+}
+
+func syntaxAnalysis(query string) (err error, n int) {
+	return nil, len(query)
+}
+
+func parseQuery(query string) StructuredQuery {
+	fmt.Println("DEBUG: Parsed query ----- ", query)
+	return StructuredQuery{}
+}
+
+func executeQuery(query StructuredQuery) (error, ExecutionResult) {
+	fmt.Println("DEBUG: Execution in progress ...", query)
+	return nil, ExecutionResult{status: "success"}
+}
+
 func handleQuery(query string) {
 	fmt.Println("processing query ...", query)
+	if err, queryLength := syntaxAnalysis(query); err == nil {
+		fmt.Println("Length of query =", queryLength)
+		structuredQuery := parseQuery(query)
+		possibleExecutionError, executionResult := executeQuery(structuredQuery)
+		if possibleExecutionError != nil {
+			fmt.Println(possibleExecutionError)
+		} else {
+			fmt.Println(executionResult.getDebugMessage())
+		}
+	} else {
+		fmt.Println(err)
+	}
 }
