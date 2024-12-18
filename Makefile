c7b220d458e39b1b36b8ffe7cd3725bd3cb08ef9
sklion <sktvpx@gmail.com>
2024-12-18 19:46:24 +0300

    Creating test with simulation of several clients...

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ run: build
 
 test:
 	@go test ./... -v
+
+run-several-clients-test:
+	@go test -v mypostgres/test/highload -run TestConnectSeveralClientsToDatabase
--- test/highload/main_test.go
+++ test/highload/main_test.go
@@ -2,15 +2,40 @@ package highload
 
 import (
 	"fmt"
+	"os/exec"
 	"testing"
 )
 
 func init() {
 	fmt.Println("init function...")
+	/*
+		Launch several clients in init function? Via telnet? Is it possible?
+	*/
 }
 
-func ConnectSeveralClientsToDatabase(t *testing.T) {
+func TestConnectSeveralClientsToDatabase(t *testing.T) {
 	/*
 		Testing gopostgres behaviour when two clients are trying to connect and execute some query.
 	*/
+
+	fmt.Println("test print")
+	app := "telnet"
+	q := "localhost"
+	w := "5555"
+
+	cmd := exec.Command(app, q, w)
+	stdout, err := cmd.Output()
+	fmt.Println(string(stdout))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// go func() {
+	// 	time.Sleep(time.Hour)
+	// 	fmt.Print("qwe qw")
+	// }()
+
+	fmt.Println(string(stdout))
 }
